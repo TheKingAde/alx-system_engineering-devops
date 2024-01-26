74e50a8a853853991bddb2503b1a8c0aa9ce431c
ADE <theking-ade@outlook.com>
2024-01-25 19:12:23 -0800

    fix file 7

--- 0x0C-web_server/7-puppet_install_nginx_web_server.pp
+++ 0x0C-web_server/7-puppet_install_nginx_web_server.pp
@@ -1,40 +1,41 @@
+# This puppet class configures a server
 class nginx_install {
-    package { 'nginx':
-        ensure => installed,
-    }
+  package { 'nginx':
+    ensure => installed,
+  }
 
-    service { 'nginx':
-        ensure     => running,
-        enable     => true,
-        require    => Package['nginx'],
-    }
+  service { 'nginx':
+    ensure  => running,
+    enable  => true,
+    require => Package['nginx'],
+  }
 
-    file { '/var/www/html/index.html':
-        ensure  => file,
-        content => 'Hello World!',
-        require => Package['nginx'],
-    }
+  file { '/var/www/html/index.html':
+    ensure  => file,
+    content => 'Hello World!',
+    require => Package['nginx'],
+  }
 
-    file { '/etc/nginx/sites-available/default':
-        ensure  => file,
-        content => "
+  file { '/etc/nginx/sites-available/default':
+    ensure  => file,
+    content => "
 server {
-    listen 80 default_server;
-    listen [::]:80 default_server;
-    root /var/www/html;
-    index index.html;
-    server_name _;
-    location / {
-        try_files \$uri \$uri/ =404;
-    }
-    location /redirect_me {
-        return 301 \$scheme://\$host;
-    }
+  listen 80 default_server;
+  listen [::]:80 default_server;
+  root /var/www/html;
+  index index.html;
+  server_name _;
+  location / {
+    try_files \$uri \$uri/ =404;
+  }
+  location /redirect_me {
+    return 301 \$scheme://\$host;
+  }
 }
 ",
-        notify  => Service['nginx'],
-        require => Package['nginx'],
-    }
+    notify  => Service['nginx'],
+    require => Package['nginx'],
+  }
 }
 
 include nginx_install

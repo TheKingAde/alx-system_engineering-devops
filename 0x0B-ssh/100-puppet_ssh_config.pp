21e25822aff5238fcbff36145551511dbfeae26c
ADE <theking-ade@outlook.com>
2024-03-16 05:34:16 -0700

    update file 100

--- 0x0B-ssh/100-puppet_ssh_config.pp
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -1,9 +1,16 @@
+# Define a class for managing SSH client configuration
+class { 'ssh::client': }
+
+# Configure IdentityFile
 file_line { 'Declare identity file':
   path => '/etc/ssh/ssh_config',
   line => 'IdentityFile ~/.ssh/school',
+  ensure => present,
 }
 
+# Disable PasswordAuthentication
 file_line { 'Turn off passwd auth':
   path => '/etc/ssh/ssh_config',
   line => 'PasswordAuthentication no',
+  ensure => present,
 }
